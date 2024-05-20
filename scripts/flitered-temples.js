3e60a159daa0f72b65c22fdaec3e85f4393f540b
Abdias <abdiasbrevil@gmail.com>
2024-05-19 22:05:36 -0400

    Album Temple

--- scripts/flitered-temples.js
+++ scripts/flitered-temples.js
@@ -1,26 +1,13 @@
-// Navigation menu
 
-const hamButton = document.querySelector('#menu');
 
-const navigation = document.querySelector('.navigation');
-const main = document.querySelector("main");
 
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
 
-    
-        if (modeButton.textContent.includes("☰")) {
-            main.style.background = "#000";
-            main.style.color = "#fff";
-            modeButton.textContent = "❌";
-        } else {
-            main.style.background = "#eee";
-            main.style.color = "#000";
-            modeButton.textContent = "☰";
-        }
-    });
-;
+
+
+
+
+
+
 
 const temples = [
     {
@@ -104,5 +91,3 @@ const temples = [
 
 
 
-
-  
