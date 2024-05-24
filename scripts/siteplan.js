7d86a46d831d8dc88fc425a162fe15014dc5d56d
Abdias <abdiasbrevil@gmail.com>
2024-05-24 15:12:18 -0400

    siteplan

--- scripts/siteplan.js
+++ scripts/siteplan.js
@@ -1,3 +1,26 @@
 document.getElementById('menu').addEventListener('click', function() {
     document.querySelector('.navigation').classList.toggle('show')
 }) 
+
+const hamButton = document.querySelector('#menu');
+
+const navigation = document.querySelector('.navigation');
+const main = document.querySelector("main");
+
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+
+    
+        if (modeButton.textContent.includes("☰")) {
+            main.style.background = "#000";
+            main.style.color = "#fff";
+            modeButton.textContent = "❌";
+        } else {
+            main.style.background = "#eee";
+            main.style.color = "#000";
+            modeButton.textContent = "☰";
+        }
+    });
+;
