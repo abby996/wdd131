46db4f9dc0f8595f73210ec27148bfd577e19eef
Abdias <abdiasbrevil@gmail.com>
2024-05-28 11:33:47 -0400

    form

--- scripts/form.js
+++ scripts/form.js
@@ -1,28 +1,28 @@
 const products = [
     {
-      id: fc-1888,
-      name: "flux capacitor",
-      averagerating: 4.5
+      id: 'fc-1888',
+    name: 'flux capacitor',
+    'avg-rating': 4.5
     },
     {
-      id: fc-2050,
-      name: "power laces",
-      averagerating: 4.7,
+      id: 'fc-2050',
+    name: 'power laces',
+    avgRating: 4.7
     },
     {
-      id: fs-1987,
-      name: "time circuits",
-      averagerating: 3.5
+      id: 'fs-1987',
+    name: 'time circuits',
+    avgRating: 3.5
     },
     {
-      id: ac-2000,
-      name: "low voltage reactor",
-      averagerating: 3.9
+      id: 'ac-2000',
+    name: 'low voltage reactor',
+    avgRating: 3.9
     },
     {
-      id: jj-1969,
-      name: "warp equalizer",
-      averagerating: 5.0
+      id: 'jj-1969',
+    name: 'warp equalizer',
+    avgRating: 5.0
     }
   ];
 
